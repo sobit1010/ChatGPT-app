86007d7b0a54e2ac0a5c2cab988269299bdff57e
sobit1010 <mailforsobit@gmail.com>
2023-03-23 12:46:28 +0530

    url changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,7 @@ function App(){
 
   const fetchBotResponse = async () => {
       const { data } = await axios.post(
-           "http://localhost:4000", 
+        "https://sobit-chatgpt.onrender.com", 
         { input },
         {
             headers: {
